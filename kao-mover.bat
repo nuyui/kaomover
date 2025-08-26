4fa932518fffb8bc7e84637bae304d948a6f9b5c
nuyai <emofurrygayporn1337@gmail.com>
2025-08-26 15:40:18 -0700

    Update kao-mover.bat

--- kao-mover.bat
+++ kao-mover.bat
@@ -1,10 +1,11 @@
 @echo off
 :: download latest version from GitHub
 
-set "downloadUrl=https://raw.githubusercontent.com/nuyui/kao-mover/main/kaomover.bat"
+set "downloadUrl=https://github.com/nuyui/kaomover/blob/main/kaomover/kaomover.bat"
 set "tempFile=%temp%\kaomover.bat"
 
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%downloadUrl%', '%tempFile%')"
 
 :: run the downloaded version
+
 call "%tempFile%"
